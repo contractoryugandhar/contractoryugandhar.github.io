42673b0598d8c1d2e55d7151d4722d01e2a31ecb
prateep.gedupudi <prateep.gedupudi@gmail.com>
2014-11-25 22:25:03 +0530

    changed controler with ctrl

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 var myApp=angular.module('myApp',[]);
 
-myApp.controller('PortfolioController',function($scope,$http){
+myApp.controller('PortfolioCtrl',function($scope,$http){
 	$http.get('model/portfolio.json').success(function(data){
 		$scope.portfolio=data;
 	});
